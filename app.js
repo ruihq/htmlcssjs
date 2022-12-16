0425e23dc5d666061e7097edd6d32a037a5710a5
Kylee ;) <120704613+kyleearroyo@users.noreply.github.com>
2022-12-15 23:14:06 -0800

    bread

---
+++ app.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("amoled-mode");
+}
+
+function doSomething() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+function blue() {
+    var element = document.body;
+    element.classList.toggle("blue-mode");
+}
